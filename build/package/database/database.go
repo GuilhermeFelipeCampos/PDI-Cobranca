b066a63f2dce5d29b168da5499b46ca436c439e2
Guilherme Felipe Campos <guilherme.campos@ifood.com.br>
2022-11-16 18:12:03 -0300

    Feat/Creating Repository Users

--- build/package/database/database.go
+++ build/package/database/database.go
@@ -4,14 +4,16 @@ import (
 	"PDI-COBRANCA/build/package/configs"
 	"database/sql"
 	"fmt"
+
+	_ "github.com/lib/pq"
 )
 
 var cfg = configs.ConfigsDB{}
 
 func OpenConn() (*sql.DB, error) {
 
-	conn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", cfg.DBHost, cfg.DBPort, cfg.DBUser, cfg.DBPassword, cfg.DBName)
-	db, err := sql.Open(cfg.DBUser, conn)
+	conn := fmt.Sprint("host=localhost port=5432 user=postgres password=admin dbname=pdi_cobranca_db sslmode=disable")
+	db, err := sql.Open("postgres", conn)
 	if err != nil {
 		panic(err)
 	}
--- build/package/main.go
+++ build/package/main.go
@@ -3,19 +3,23 @@ package main
 import (
 	"PDI-COBRANCA/build/package/handlers"
 	"PDI-COBRANCA/build/package/server"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
+
 	//find all
 	server.E.GET("/products", handlers.GetAll)
-	//find by id
-	server.E.GET("/products/:id", handlers.GetById)
+	//find by email
+	server.E.GET("/product", handlers.GetByEmail)
 	// record products
 	server.E.POST("/products", handlers.Create)
 	// update product
 	server.E.PUT("/products/:id", handlers.Update)
 	//delete by id
 	server.E.DELETE("/products/:id", handlers.Delete)
+
 	server.StartServer()
 
 }
